875c7234162bef74865e60a1cfa1fb70db91ec6b
Shone Wang <Shone.Wang@morningstar.com>
2017-02-23 21:02:17 -0600

    add filter for searching ticker

--- stock_events/js/tickerList.js
+++ stock_events/js/tickerList.js
@@ -1,49 +1,72 @@
 var TickerList = function (el) {
 	this.$el = $(el);
 	this.ticker = request.getQuerystring('ticker', 'IBM').toUpperCase();
-	this.tickerListSrc = '/js/MockData/SP500.js';
+	this.tickerListSrc = '/js/MockData/SP500.js',
+	this.securityList = [];
 }
 TickerList.prototype = {
 	render: function (filter) {
 		var _this = this;
 		$.getScript(this.tickerListSrc, function(data, textStatus, jqxhr) {
-            if (textStatus === 'success' && SP500List) {                
-                _this.generateTickerHTMLContent(SP500List, filter);
+            if (textStatus === 'success' && SP500List) {
+            	_this.securityList = SP500List;                
+                _this.generateTickerHTMLContent( );
                 _this.addActiveClass();
                 _this.bindEvent();
             }
         });
 	},
-	generateTickerHTMLContent: function (data, filter) {
-		var len = data.length,
+	generateTickerHTMLContent: function (filter) {
+		var _this = this,
+			len = _this.securityList.length,
 			item = {},
 			tickerContent = '',
 			i = 0;
 
 		for (i = 0; i < len; i++) {
-			item = data[i];
-
+			item = _this.securityList[i];
+			if(filter && item.Ticker.toLowerCase().indexOf(filter.toLowerCase()) === -1) {	
+				continue;			
+			}
 			tickerContent += '<button type="button" class="ticker-item" data-ticker="';
 
 			// Ticker
 			tickerContent += item.Ticker;
 
-			tickerContent += '">' + item.Name + '</button>';;
+			tickerContent += '">' + item.Name + '</button>';
 		}
 
-		this.$el.empty().html(tickerContent)
+		if(tickerContent === '') {
+			tickerContent = '<p class="left-ticker-panel-no-ticker">Ticker is not available right now, please try it again later!</p>'
+		}
+
+		this.$el.find('.left-ticker-panel-securities').empty().html(tickerContent)
 	},
 	addActiveClass: function () {
 		$('[data-ticker="' + this.ticker + '"]', this.$el).addClass('active-ticker')
 	},
 	bindEvent: function () {
-		var _this = this;
-		_this.$el.find('button').on('click', function () {
-			if($(this).data('ticker') !== _this.ticker) {
-				_this.$el.find('.active-ticker').removeClass('active-ticker');
-				$(this).addClass('active-ticker');
-				_this.$el.trigger('change-ticker');
+		var _this = this,
+			$filter = _this.$el.find('.ticker-filter'),
+			filterCharacters = '',
+			$buttonItem = null;
+		_this.$el.on('click', function (e) {
+			$buttonItem = $(e.target);
+			if($buttonItem.hasClass('ticker-item')){
+				if ($buttonItem.data('ticker') !== _this.ticker) {
+					_this.$el.find('.active-ticker').removeClass('active-ticker');
+					$buttonItem.addClass('active-ticker');
+					_this.$el.trigger('change-ticker');
+				}
+			}			
+		});
+
+		$filter.on('keyup', function () {
+			filterCharacters = $filter.val();
+			if(filterCharacters === '') {
+				return;
 			}
+			_this.generateTickerHTMLContent(filterCharacters)
 		});
 	}
 }
